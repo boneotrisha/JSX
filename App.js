d3d9fcbb38911a5999e42f1605af5ca7e60149dd
boneotrisha <163127317+boneotrisha@users.noreply.github.com>
2024-06-17 12:56:00 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,22 +1,26 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Main from './pages/Main';
 import About from './pages/About';
 import Menu from './pages/Menu';
-import Main from './pages/Main';
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import './css/s.css';
+import './css/about.css';
+import './css/icons.css';
+import './css/menu.css';
+import './css/contact.css';
+
 
 function App() {
-    return (
-        <BrowserRouter>
-        <Routes>
-
-            <Route path='/' element={<Main/>}/>
-            <Route path='/About' element={<About/>}/>
-            <Route path='/Menu' element={<Menu/>}/>
-
-        </Routes>
-        </BrowserRouter>
-
-        
-     )
+  return (
+    <BrowserRouter>
+     
+      <Routes>
+        <Route path="/Main" element={<Main />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Menu" element={<Menu />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
+
 export default App;
