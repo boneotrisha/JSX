c41c530a8a9425082991bc5bd4cdece59d2ea8d4
boneotrisha <163127317+boneotrisha@users.noreply.github.com>
2025-02-11 20:00:55 +0800

    Add files via upload

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbName = "bombeo_boneo";
+$conn = new mysqli($servername, $username, $password, $dbName);
+
+if ($conn -> connect_error) {
+
+}
+
+
+?>
---
+++ newindex.php
@@ -0,0 +1,12 @@
+<?php
+
+include("./dB/config.php");
+include("./includes/header.php");
+include("./includes/topbar.php");
+include("./includes/sidebar.php");
+
+?>
+
+<?php
+include("./includes/footer.php");
+?>
